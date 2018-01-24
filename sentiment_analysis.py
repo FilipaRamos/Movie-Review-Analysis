604e9a8e2701614e9a496dfea4d23d37ae3044ad
Clara Oozeer <Clara@luckeyhomes.com>
2018-01-24 12:06:23 +0200

    save best model with Keras

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, Activation, BatchNormalization, Conv1D, Flatten, MaxPooling1D, Embedding
 from keras import optimizers, regularizers
+from keras.callbacks import ModelCheckpoint
 
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
@@ -63,7 +64,7 @@ def mlp_network():
 
 ### Convolutional neural network ###
 
-def conv_network(encoding, vocab_size, max_length, vector_size=0):
+def model_to_complile(encoding, vocab_size, max_length, vector_size=0):
     '''
     encoding: 'discrete' or 'word2vec'
     vector_size: size of the word2vec embedding
@@ -100,7 +101,7 @@ def conv_network(encoding, vocab_size, max_length, vector_size=0):
     model.add(BatchNormalization())
     model.add(Dropout(0.5))
     model.add(Dense(1, activation="sigmoid"))
-    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+    #model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
     
     return model
 
@@ -121,21 +122,20 @@ def lstm_model():
 
 ### Train models and get results ###
 
-def train_embedding_model(X_encoded, y, vocab_size, max_length, method, epochs, vector_size=0):
+def train_model(X_encoded, y, vocab_size, max_length, filepath, method, epochs, vector_size=0):
     '''
-    method: discrete or word2vec
+    method: 'discrete' or 'word2vec'
     '''
-    train_data, test_data, train_labels, test_labels = train_test_split(X_encoded, y
-                                                                                , test_size=0.2, shuffle=False)
-    model = conv_network(method, vocab_size, max_length, vector_size)
+    # split dataset
+    train_data, test_data, train_labels, test_labels = train_test_split(X_encoded, y, test_size=0.2, shuffle=False)
+    # create model
+    model = model_to_complile(method, vocab_size, max_length, vector_size)
+    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+    # checkpoint and history 
+    checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
     history = tools.AccuracyLossHistory((test_data, test_labels))
-    model.fit(train_data, train_labels, epochs=epochs, batch_size=64, verbose=2, callbacks=[history])
-    
-    tools.plot_params(history.train, history.val)
-    
-    index = np.argmax(np.asarray(history.val).T[1])
-    print('** Best ** \nValidation loss %.2f \nValidation accuracy %.2f' % (history.val[index][0], history.val[index][1]*100))
-    print('\n** Last ** \nValidation loss %.2f \nValidation accuracy %.2f' % (history.val[epochs-1][0], history.val[epochs-1][1]*100))
+    # fit the model
+    model.fit(train_data, train_labels, validation_split=0.2, epochs=epochs, batch_size=64, callbacks=[history, checkpoint], verbose=0)
     
     return history, [train_data, test_data, train_labels, test_labels]
 
@@ -148,23 +148,47 @@ def inverse_to_categorical(array):
             res.append(0)
     return res
 
+def apply_best_model(data, filepath, method, vocab_size, max_length, vector_size=0):
+    # create model
+    model = model_to_complile(method, vocab_size, max_length, vector_size)
+    # load weights
+    model.load_weights(filepath)
+    # compile model
+    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+    print("Created model and loaded weights from file")
+
+    # estimate accuracy on the test dataset using loaded weights
+    scores = model.evaluate(data[1], data[3], verbose=0)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    print("%s: %.4f" % (model.metrics_names[0], scores[0]))
+    # predict test labels and print confusion matrix
+    yprob = model.predict(data[1])
+    ypred = inverse_to_categorical(yprob)
+    print('\nConfusion Matrix:\n', np.round(confusion_matrix(data[3], ypred)*100/len(data[3]),2))
+    
+    return scores, ypred
+
 # One-hot encoding
 
 X_onehot = pr.onehot_encoding(X_clean, vocab_size, max_length)
-history_oh, data_oh = train_embedding_model(X_onehot, y, vocab_size, max_length, 'discrete', 12)
+filepath_oh = "weights-improvement-oh.hdf5"
+method_oh = 'discrete'
+epochs_oh = 15
 
-yprob_oh = history_oh.model.predict(data_oh[1])
-ypred_oh = inverse_to_categorical(yprob_oh)
-print('Confusion Matrix:\n', np.round(confusion_matrix(data_oh[3], ypred_oh)*100/len(data_oh[3]),2))
+history_oh, data_oh = train_model(X_onehot, y, vocab_size, max_length, filepath_oh, method_oh, epochs_oh)
+tools.plot_params(history_oh.train, history_oh.val)
+scores_oh, ypred_oh = apply_best_model(data_oh, filepath_oh, method_oh, vocab_size, max_length)
 
 # Hashing encoding
 
 X_hash = pr.hash_encoding(X_clean, vocab_size, max_length)
-history_hs, data_hs = train_embedding_model(X_hash, y, vocab_size, max_length, 'discrete', 15)
+filepath_hs = "weights-improvement-hs.hdf5"
+method_hs = 'discrete'
+epochs_hs = 15
 
-yprob_hs = history_hs.model.predict(data_hs[1])
-ypred_hs = inverse_to_categorical(yprob_hs)
-print('Confusion Matrix:\n', np.round(confusion_matrix(data_hs[3], ypred_hs)*100/len(data_hs[3]),2))
+history_hs, data_hs = train_model(X_onehot, y, vocab_size, max_length, filepath_hs, method_hs, epochs_hs)
+tools.plot_params(history_hs.train, history_hs.val)
+scores_hs, ypred_hs = apply_best_model(data_hs, filepath_hs, method_hs, vocab_size, max_length)
 
 # Word2Vec embedding (skipgram)
 
@@ -178,8 +202,10 @@ workers = 2
 iter = 40
 
 X_word2vec = pr.word2vec_embeding(X_clean, max_length, min_count, vector_size, window, negative, hs, workers, sg, iter)
-history_w2v, data_w2v = train_embedding_model(X_word2vec, y, vocab_size, max_length, 'word2vec', 12, vector_size)
+filepath_w2v = "weights-improvement-w2v.hdf5"
+method_w2v = 'word2vec'
+epochs_w2v = 15
 
-yprob_w2v = history_w2v.model.predict(data_w2v[1])
-ypred_w2v = inverse_to_categorical(yprob_w2v)
-print('Confusion Matrix:\n', np.round(confusion_matrix(data_w2v[3], ypred_w2v)*100/len(data_w2v[3]),2))
+history_w2v, data_w2v = train_model(X_word2vec, y, vocab_size, max_length, filepath_w2v, method_w2v, epochs_w2v, vector_size)
+tools.plot_params(history_w2v.train, history_w2v.val)
+scores_w2v, ypred_w2v = apply_best_model(data_w2v, filepath_w2v, method_w2v, vocab_size, max_length, vector_size)
